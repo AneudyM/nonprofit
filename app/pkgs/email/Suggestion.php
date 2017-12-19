ace080208883cdf3ed7ba359f30c8a9e488ca01f
Aneudy Mota <aneudy.motacatalino@gmail.com>
2017-12-18 20:36:52 -0500

    Add Facebook timeline, like, and share features

---
+++ app/pkgs/email/Suggestion.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: aneudy
+ * Date: 17/12/17
+ * Time: 3:27 PM
+ *
+ * This class handles the Suggestion messages sent via the
+ * Suggestions mailbox form linked from the footer of the page.
+ *
+ */
+
+class Suggestion {
+  private $area;
+  private $suggestion;
+
+  public function setArea($area) {
+    $this->area = $area;
+  }
+
+  public function setSuggestion($suggestion) {
+    $this->suggestion = $suggestion;
+  }
+
+  public function Area() {
+    return $this->area;
+  }
+
+  public function SuggestionMsg() {
+    return $this->suggestion;
+  }
+}
