1244f362202d4e85ad04050d42bde493fa164609
Aneudy Mota <aneudy.motacatalino@gmail.com>
2018-01-07 11:59:35 -0500

    Suggestion and Service Request major improvement

--- app/config/dbsettings.php
+++ app/config/dbsettings.php
@@ -6,10 +6,10 @@
  * Time: 5:10 PM
  */
 
-$user = "intelycs_dr_adm";
-$pass = "djb5944014";
-$host = "intelycs.com";
-$db = "intelycs_dr_adm";
+$user = "username";
+$pass = "password";
+$host = "hostname";
+$db = "database";
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 if ($mysqli->connect_errno) {
